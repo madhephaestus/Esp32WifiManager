3faf3f74f75255addd4949b93dcd751bac84813e
Kevin Harrington <mad.hephaestus@gmail.com>
2018-03-12 14:29:47 -0400

    Example robot

--- examples/ExampleRobot/ExampleRobot.ino
+++ examples/ExampleRobot/ExampleRobot.ino
@@ -2,10 +2,15 @@
 #define AP_SSID  "RBE_Team_1"       //can set ap hostname here
 #define AP_PW	 "thisissecret"       //can set ap hostname here
 AbstractController * gameController=NULL;
+Servo m1;
+Servo m2;
 //The setup function is called once at startup of the sketch
 void setup()
 {
 	launchControllerReciver(AP_SSID,AP_PW);
+	Serial.println("Attaching servos...");
+	m1.attach(2);
+	m2.attach(15);
 }
 
 // The loop function is called in an endless loop
@@ -16,6 +21,8 @@ void loop()
 	if(gameController!=NULL){
 		//user code
 		Serial.println("Code running..");
+		m1.write((int)(gameController->getData()[1]<<2));
+		m2.write((int)(gameController->getData()[3]<<3));
 
 	}else
 		gameController=getUdpController(2);
