6c72cc5e87f1342cb51da14820f389b0000d8b24
Kevin Harrington <mad.hephaestus@gmail.com>
2019-04-02 15:40:29 -0400

    Moving wifi managet to library

--- examples/WifiManagerExample/WifiManagerExample.ino
+++ examples/WifiManagerExample/WifiManagerExample.ino
@@ -1,8 +1,7 @@
 #include <Preferences.h>
 #include <WiFi.h>
-#include <SimplePacketComs.h>
-#include <Esp32SimplePacketComs.h>
-#include <wifi/WifiManager.h>
+#include <Esp32WifiManager.h>
+
 //Create a wifi manager
 WifiManager manager;
 // modify the preferences
